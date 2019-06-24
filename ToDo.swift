f7f52c57388d77279bc725b9745a519d05c5a0ff
Shirui Zhong <apple@Apples-MacBook-Pro-66.local>
2019-06-24 13:25:28 -0500

    complete iteration 0

---
+++ ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Apple on 6/24/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+class ToDo{
+    var name = ""
+    var important = false
+}
